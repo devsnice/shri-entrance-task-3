f16459d486878cc77d8d7b7ab04d1f3e07c517b4
Borisov Semen <devsnice@gmail.com>
2018-01-10 13:57:03 +0300

    [+] - init::added create-react-application

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SchedulePage from './pages/schedulePage/schedulePage';
+
+ReactDOM.render(<SchedulePage />, document.getElementById('root'));
