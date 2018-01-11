64c75c72016579306d6777d6d24af8eaacd428ca
Borisov Semen <devsnice@gmail.com>
2018-01-11 14:20:24 +0300

    [+] - Schedule::added ScheduleFloors, added logic of transformation server data

---
+++ src/components/containers/Schedule/ScheduleFloor/ScheduleFloor.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import styled from 'styled-components';
+import media from 'styled-media-query';
+
+import { Box } from 'grid-styled';
+
+const Wrapper = styled(Box)`
+  position: relative;
+  padding-top: 21px;
+`;
+
+const FloorLabel = styled(Box)`
+  font-family: ${props => props.theme.fontBold};
+  font-size: 11px;
+  color: #858e98;
+  letter-spacing: 0.4px;
+  text-transform: uppercase;
+  position: relative;
+  will-change: left;
+
+  ${media.lessThan('small')`
+    padding-left: 16px;
+  `};
+`;
+
+const FloorRooms = styled(Box)`
+  margin-top: 8px;
+`;
+
+class ScheduleFloor extends Component {
+  static propTypes = {
+    floorNumber: PropTypes.number.isRequired,
+    rooms: PropTypes.arrayOf(
+      PropTypes.shape({
+        events: PropTypes.array.isRequired,
+        title: PropTypes.string.isRequired,
+        capacity: PropTypes.number.isRequired,
+        id: PropTypes.string.isRequired
+      }).isRequired
+    ).isRequired
+  };
+
+  static defaultProps = {};
+
+  render() {
+    const { floorNumber, rooms } = this.props;
+
+    return (
+      <Wrapper>
+        <FloorLabel>{floorNumber}</FloorLabel>
+        <FloorRooms>
+          <Box />
+        </FloorRooms>
+      </Wrapper>
+    );
+  }
+}
+
+export default ScheduleFloor;
---
+++ src/components/containers/Schedule/ScheduleFloors/ScheduleFloors.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import styled from 'styled-components';
+import media from 'styled-media-query';
+
+import { Box } from 'grid-styled';
+
+import ScheduleFloor from '../ScheduleFloor/ScheduleFloor';
+
+const Wrapper = styled(Box)`
+  height: calc(100% - 47px);
+  width: 100%;
+  position: relative;
+`;
+
+const StagesUnderlayer = styled(Box)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 245px;
+  background: #fff;
+  height: 100%;
+
+  ${media.lessThan('small')`
+    display: none;
+  `};
+`;
+
+class ScheduleFloors extends Component {
+  static propTypes = {
+    floors: PropTypes.arrayOf(
+      PropTypes.shape({
+        rooms: PropTypes.array.isRequired
+      }).isRequired
+    )
+  };
+
+  static defaultProps = {
+    floors: []
+  };
+
+  renderFloors = () => {
+    const { floors } = this.props;
+
+    return floors.map(floor => (
+      <ScheduleFloor key={floor.floorNumber} {...floor} />
+    ));
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        {this.renderFloors()}
+
+        <StagesUnderlayer />
+      </Wrapper>
+    );
+  }
+}
+
+export default ScheduleFloors;
