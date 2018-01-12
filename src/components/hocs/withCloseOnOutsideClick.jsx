64813551f52baad004bf6f0d8cf1332135d6b016
Borisov Semen <devsnice@gmail.com>
2018-01-12 15:35:58 +0300

    [+] - hocs::added hocs for closing components, when there was click outside of its

---
+++ src/components/hocs/withCloseOnOutsideClick.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { doesNodeContainClick } from '../../utils/domUtils';
+
+const withCloseOnOutsideClick = WrappedComponent => {
+  return class ComponentWithCloseOnOutsideClick extends React.Component {
+    static propTypes = {
+      isOpen: PropTypes.bool.isRequired,
+      handleClose: PropTypes.func.isRequired
+    };
+
+    componentWillReceiveProps(nextProps) {
+      if (nextProps.isOpen && !this.props.isOpen) {
+        this.attachHandlersOnOpen();
+      } else if (!nextProps.isOpen && this.props.isOpen) {
+        this.dettachHandlersOnClose();
+      }
+    }
+
+    attachHandlersOnOpen = () => {
+      document.addEventListener('click', this.closeOnDocumentClick);
+    };
+
+    dettachHandlersOnClose = () => {
+      document.removeEventListener('click', this.closeOnDocumentClick);
+    };
+
+    closeOnDocumentClick = e => {
+      if (doesNodeContainClick(this.ref, e)) return;
+
+      this.props.handleClose();
+    };
+
+    handleRef = elem => (this.ref = elem);
+
+    render() {
+      return (
+        <div ref={this.handleRef}>
+          <WrappedComponent {...this.props} />
+        </div>
+      );
+    }
+  };
+};
+
+export default withCloseOnOutsideClick;
