4627088b75f15402c382eacd0afb6650ca082d72
Borisov Semen <devsnice@gmail.com>
2018-01-14 14:54:03 +0300

    [+] - components::added recommendation rooms

---
+++ src/components/containers/EventEditor/EventEditorForm/RecommendationRooms/RecommendationRoom/RecommendationRoom.jsx
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import styled, { css } from 'styled-components';
+import media from 'styled-media-query';
+
+import { Box } from 'grid-styled';
+import Icon from '../../../../../units/Icons/Icon';
+
+const cssRecommendationDefault = css`
+  background: #e9ecef;
+  color: #000;
+
+  &:hover {
+    background: #dde0e4;
+  }
+
+  &:focus {
+    background: #c8cdd3;
+  }
+`;
+
+const cssRecommendationActive = css`
+  background: #007dff;
+  color: #fff;
+
+  &:hover {
+    background: #0059ff;
+  }
+
+  &:focus {
+    background: #0059ff;
+  }
+`;
+
+const RecommendationBox = styled(Box)`
+  ${props =>
+    props.active ? cssRecommendationActive : cssRecommendationDefault};
+
+  cursor: pointer;
+  border-radius: 4px;
+  height: 37px;
+  line-height: 37px;
+  margin-bottom: 8px;
+  padding: 0 10px;
+  position: relative;
+  width: 100%;
+  text-align: left;
+
+  ${media.lessThan('small')`
+    height: 44px;
+    line-height: 44px;
+  `};
+`;
+
+const RecommendationData = styled(Box)`
+  font-size: 13px;
+
+  ${media.lessThan('small')`
+    font-size: 15px;
+ `};
+`;
+
+const RecommendationRoomTime = styled.span`
+  font-family: ${props => props.theme.fontBold};
+  margin-right: 16px;
+  display: inline-block;
+`;
+
+const RecommendationRoomName = styled.span`
+  font-family: ${props => props.theme.fontDefault};
+`;
+
+const RecommendationRoomRemove = styled.div`
+  position: absolute;
+  right: 12px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+`;
+
+class RecommendationRoom extends Component {
+  static PropTypes = {
+    isActive: PropTypes.bool.isRequired,
+    room: PropTypes.object.isRequired,
+    handleSelectItem: PropTypes.func.isRequired,
+    handleRemoveSelection: PropTypes.func.isRequired
+  };
+
+  render() {
+    const { room, isActive } = this.props;
+
+    return (
+      <RecommendationBox onClick={this.handleSelectItem} active={isActive}>
+        <RecommendationData>
+          <RecommendationRoomTime>10:00 - 11:00</RecommendationRoomTime>
+          <RecommendationRoomName>
+            {`${room.title} · ${room.floor} этаж`}
+          </RecommendationRoomName>
+
+          {isActive && (
+            <RecommendationRoomRemove onClick={this.handleRemoveSelection}>
+              <Icon iconName="close" />
+            </RecommendationRoomRemove>
+          )}
+        </RecommendationData>
+      </RecommendationBox>
+    );
+  }
+}
+
+export default RecommendationRoom;
---
+++ src/components/containers/EventEditor/EventEditorForm/RecommendationRooms/RecommendationRooms.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import styled from 'styled-components';
+
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import { Box } from 'grid-styled';
+
+import Label from '../../../../units/Label/Label';
+
+import RecommendationRoom from './RecommendationRoom/RecommendationRoom';
+
+const RoomsQuery = gql`
+  query {
+    rooms {
+      id
+      title
+      capacity
+      floor
+    }
+  }
+`;
+
+class RecommendationRooms extends Component {
+  static propTypes = {
+    value: PropTypes.string
+  };
+
+  static defaultProps = {
+    value: null
+  };
+
+  handleSelectItem = id => {};
+
+  handleRemoveSelection = id => {};
+
+  // TODO: add error fetching, move this logic outside
+  getRoomsList = () => {
+    const { data } = this.props;
+
+    if (data.networkStatus === 7 && !data.error) {
+      return data.rooms;
+    }
+
+    return [];
+  };
+
+  findRecommendations = () => {
+    const allRooms = this.getRoomsList();
+
+    return allRooms;
+  };
+
+  renderRecommendations = rooms => {
+    return rooms.map(room => (
+      <RecommendationRoom
+        isActive={room.id === this.props.value}
+        handleSelectItem={this.handleSelectItem}
+        handleRemoveSelection={this.handleRemoveSelection}
+        key={room.id}
+        room={room}
+      />
+    ));
+  };
+
+  render() {
+    const recommendationRoomsList = this.findRecommendations();
+
+    if (!recommendationRoomsList.length) {
+      return null;
+    }
+
+    return (
+      <Box>
+        <Label label="Рекомендованные переговорки" />
+        {this.renderRecommendations(recommendationRoomsList)}
+      </Box>
+    );
+  }
+}
+
+export default graphql(RoomsQuery)(RecommendationRooms);
